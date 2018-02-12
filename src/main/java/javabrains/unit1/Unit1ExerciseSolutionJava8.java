9ff46cf9874d3eaf66dd0538d78d216f448dc605
HubsonT <hubsonpanter@gmail.com>
2018-02-12 23:23:28 +0100

    minor changes in file Unit1ExerciseSolutionJava8

--- src/main/java/javabrains/unit1/Unit1ExerciseSolutionJava8.java
+++ src/main/java/javabrains/unit1/Unit1ExerciseSolutionJava8.java
@@ -16,12 +16,12 @@ public class Unit1ExerciseSolutionJava8 {
             new Person("Matthew", "Arnold", 39));
 	
         System.out.println("Before sorting:");
-        printAll(people);
+        printConditionally(people, p -> true);
     
 // Step 1 - sort list by last name:
 	Collections.sort(people, (p1,p2) -> p1.getLastName().compareTo(p2.getLastName()));
         System.out.println("\nAfter sorting by last name(ascending):");
-        printAll(people);
+        printConditionally(people, p -> true);
                 
         System.out.println("\nPrint conditionally - all the people that have last name beginning with C");
         printConditionally(people, p -> p.getLastName().startsWith("C"));
@@ -30,15 +30,7 @@ public class Unit1ExerciseSolutionJava8 {
         printConditionally(people, p -> p.getFirstName().startsWith("C"));
         
 }
-
-// Step 2 - create a method that prints all the elements in the list
-    private static void printAll(List<Person> people) {
-        for(Person p: people){
-            System.out.print(p);
-        }
-    }
-    
-// Step 3 - create a method that prints all the people conditionally
+// Step 2, 3 - create a method that prints all the people conditionally
     private static void printConditionally(List<Person> people, Condition condition) {
         for (Person person : people) {
             if (condition.test(person)) {
