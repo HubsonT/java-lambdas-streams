20efbcb43b045f47430b9834432848d52033eefe
HubsonT <hubsonpanter@gmail.com>
2018-02-17 15:10:52 +0100

    Lambda changed to method reference

--- src/main/java/javabrains/unit3/MethodReferenceExample2.java
+++ src/main/java/javabrains/unit3/MethodReferenceExample2.java
@@ -24,25 +24,15 @@ public class MethodReferenceExample2 {
 		// Step 1 - sort list by last name:
 		Collections.sort(people, (p1, p2) -> p1.getLastName().compareTo(p2.getLastName()));
 		System.out.println("\nAfter sorting by last name(ascending):");
-		performConditionally(people, p -> true, p -> System.out.println(p));
+		performConditionally(people, p -> true, System.out::println);
 
 		System.out.println("\nPrint conditionally - all the people that have last name beginning with C");
-		performConditionally(people, p -> p.getLastName().startsWith("C"), p -> System.out.println(p));
+		performConditionally(people, p -> p.getLastName().startsWith("C"), System.out::println);
 
 		System.out.println("\nPrint conditionally - all the names of the people that have first name beginning with C");
 		performConditionally(people, p -> p.getFirstName().startsWith("C"), p -> System.out.println(p.getFirstName()));
 
 	}
-
-	// Step 2, 3 - create a method that prints all the people conditionally
-	// private static void printConditionally(List<Person> people, Predicate<Person>
-	// predicate) {
-	// for (Person person : people) {
-	// if (predicate.test(person)) {
-	// System.out.print(person);
-	// }
-	// }
-	// }
 
 	private static void performConditionally(List<Person> people, Predicate<Person> predicate,
 			Consumer<Person> consumer) {
